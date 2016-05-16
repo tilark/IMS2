268c8dca217ef97d4728eced40bf80e8e07dc37d
tilark <tilark.liu@gmail.com>
2016-05-16 13:27:28 +0800

    ChangeTheme
    更改了主题

---
+++ IMS2/Scripts/IMSjquery.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#showitem").hide();
+    $("#hideitem").click(function () {
+        $(".pitem").hide("slow");
+        $("#showitem").show();
+        $("#hideitem").hide()
+    });
+    $("#showitem").click(function () {
+        $(".pitem").show("slow");
+        $("#hideitem").show();
+        $("#showitem").hide();
+    });
+});
