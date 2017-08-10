ac8df681e76cb0ea46457003bbcb86c8f6a0f0c3
N <bigestyjoy@163.com>
2017-08-10 15:24:09 +0800

    实现“时段时间组合算法”

---
+++ IMS2/BusinessModel/DurationTime/DurationTimeSolver.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS2.BusinessModel.DurationTime
+{
+    /// <summary>
+    /// 时段时间组合算法处理器。
+    /// </summary>
+    /// <remarks>执行“时段”、“时间”的合理组合处理。</remarks>
+    /// <see cref="时段时间组合算法"/>
+    public class DurationTimeSolver
+    {
+        /// <summary>
+        /// 初始化。
+        /// </summary>
+        public DurationTimeSolver()
+        {
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// 对指定的“时段ID”、“时间”进行组合，返回所有可能的“时段时间项”。
+        /// </summary>
+        /// <param name="durationId">时段ID。</param>
+        /// <param name="time">时间。</param>
+        /// <returns>指定的“时段ID”、“时间”所有可能的“时段时间项”集合。若输入的“时段ID”无效，或存在“年”、“半年”、“季”和“月”以外的“时段”，则返回null。</returns>
+        /// <remarks>涉及“时段ID”硬编码内容。</remarks>
+        /// <see cref="时段时间组合算法"/>
+        public List<DurationTimeItem> Solve(Guid durationId, DateTime time)
+        {
+            var returnList = new List<DurationTimeItem>();
+
+            var db = new Models.ImsDbContext();
+
+            var duration = db.Durations.Find(durationId);
+            if (duration == null)
+                return null;
+            var durations = db.Durations.Where(c => c.Level >= duration.Level).ToList();
+
+            DateTime tempTime;
+            foreach (var itemDuration in durations)
+            {
+                switch (itemDuration.DurationId.ToString().ToUpper())
+                {
+                    //年
+                    case "BA74E352-0AD5-424B-BF31-738BA5666649":
+                        tempTime = new DateTime(time.Year, 1, 1);
+                        break;
+                    //半年
+                    case "24847114-90E4-483D-B290-97781C3FA0C2":
+                        if (time.Month <= 6)
+                            tempTime = new DateTime(time.Year, 1, 1);
+                        else
+                            tempTime = new DateTime(time.Year, 7, 1);
+                        break;
+                    //季
+                    case "BD18C4F4-6552-4986-AB4E-BA2DFFDED2B3":
+                        if (time.Month <= 3)
+                            tempTime = new DateTime(time.Year, 1, 1);
+                        else if (time.Month <= 6)
+                            tempTime = new DateTime(time.Year, 4, 1);
+                        else if (time.Month <= 9)
+                            tempTime = new DateTime(time.Year, 7, 1);
+                        else
+                            tempTime = new DateTime(time.Year, 10, 1);
+                        break;
+                    //月
+                    case "D48AA438-AD71-4419-A2A2-A1C390F6C097":
+                        tempTime = new DateTime(time.Year, time.Month, 1);
+                        break;
+                    //其他
+                    default:
+                        return null;
+                }
+                returnList.Add(new DurationTimeItem { DurationId = durationId, Time = tempTime });
+            }
+
+            return returnList;
+        }
+    }
+}
