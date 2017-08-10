d61a0fb5282e92cbbe228e5634bd7ab5875c1bca
N <bigestyjoy@163.com>
2017-08-10 15:25:12 +0800

    实现“观察者模式”基础

---
+++ IMS2/BusinessModel/ObserverMode/ISubject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS2.BusinessModel.ObserverMode
+{
+    public interface ISubject
+    {
+        List<IObserver> Observers { get; set; }
+
+
+
+
+
+        void Attach(IObserver observer);
+
+        void Detach(IObserver observer);
+
+        void Notify();
+    }
+}
